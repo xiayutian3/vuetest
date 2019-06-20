b4ed3a22c9cb13275cd34735f7d5578f929054d8
eetu <1127071993@qq.com>
2019-06-20 11:40:22 +0800

    请求 用async await接收

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,31 @@
   import ajaxService from '@/api/api1.js'
 export default {
   name: 'App',
-  created(){
-    // 可以判断线上线下环境
-    // const root = process.env.API_ROOT;
-    // console.log(root);
-    ajaxService.getlist().then((res)=>{
-      console.log('res',res)
-    }).catch(err=>{
-      console.log('err',err)
-    })
+  // created(){
+  //   // 可以判断线上线下环境
+  //   // const root = process.env.API_ROOT;
+  //   // console.log(root);
+  //   ajaxService.getlist().then((res)=>{
+  //     console.log('res',res)
+  //   }).catch(err=>{
+  //     console.log('err',err)
+  //   })
+  // }
 
+
+  // 使用 async  await 也是可以的
+  async created(){
+    try{
+      let result = await ajaxService.getlist()
+      if(result.status == 200){
+        console.log('result ',result)
+      }else{
+        console.log('没有查找到数据')
+      }
+
+    }catch(err){
+      console.log('服务器出错',err)
+    }
   }
 }
 </script>
