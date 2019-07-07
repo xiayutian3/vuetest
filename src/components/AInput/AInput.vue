79f65defba3044b8c67f5db4df34198d92785ee0
1127071993@qq.com <aini2009626>
2019-07-07 11:31:29 +0800

    封装input

---
+++ src/components/AInput/AInput.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <input type="text" @input="handleInput" :value = "value">
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    value:{
+      type:[String,Number],
+      default:''
+    }
+  },
+
+  data () {
+    return {
+
+    };
+  },
+
+  created(){},
+
+  mounted(){},
+
+  methods: {
+    handleInput(event){
+      let value = event.target.value
+      this.$emit('input',value)
+    }
+  },
+
+  components: {},
+
+  computed: {}
+}
+
+</script>
+<style lang='less' scoped>
+
+</style>
---
+++ src/view/store.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+<!-- <a-input v-model="inValue" /> -->
+<!-- <a-input :value="inValue" @input = "handleInput"/> -->
+<a-input  @input = "handleInput"/>
+<p>{{inValue}}</p>
+  </div>
+</template>
+
+<script>
+import AInput from '@/components/AInput/AInput.vue'
+export default {
+  props: [],
+
+  data () {
+    return {
+      inValue:''
+    };
+  },
+
+  created(){},
+
+  mounted(){},
+
+  methods: {
+    handleInput(value){
+      this.inValue = value
+    }
+  },
+
+  components: {
+    AInput,
+  },
+
+  computed: {}
+}
+
+</script>
+<style lang='less' scoped>
+
+</style>
