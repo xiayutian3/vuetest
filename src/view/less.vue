cbafae40de9e5b339a9ae44d1fef5e0884b7973f
1127071993@qq.com <aini2009626>
2019-10-29 23:46:06 +0800

    子组件有scoped  外部还是可以作用内部的样式 例如 #app .wrap 就能覆盖 组件含有scoped 的.wrap的样式 外部相同名字的class就不能作用了，比如 都是 .wrap样式，就不能相互覆盖了

--- src/view/less.vue
+++ src/view/less.vue
@@ -10,8 +10,8 @@
   }
 </script>
 
-子组件有scoped  外部还是可以作用内部的样式 例如 #app .wrap 就能覆盖 组件含有scoped 的.wrap的样式
-  外部相同名字的class就不能作用了，比如 都是 .wrap样式，就不能相互覆盖了
+<!--子组件有scoped  外部还是可以作用内部的样式 例如 #app .wrap 就能覆盖 组件含有scoped 的.wrap的样式-->
+<!--  外部相同名字的class就不能作用了，比如 都是 .wrap样式，就不能相互覆盖了-->
 <style lang="less" scoped>
 @import "../less/index.less";
 .wrap{
