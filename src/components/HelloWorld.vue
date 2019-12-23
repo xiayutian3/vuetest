c9eab23526360f883a0995e01b00ddf49508e023
1127071993@qq.com <aini2009626>
2019-12-23 21:34:41 +0800

    v-if在组件标签上（钩子函数不执行）和组件内根元素上的区别（钩子函数执行）

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="parent" >
+<!--    v-if="false"如果写在组件上，是不执行组件里的钩子函数的，例如：<none v-if="false"></none>-->
+<!--    如果写在组件的最外层跟标签上，组件里的钩子函数是会执行的，
+        在组件里的话（隐藏整个组件），加在根标签，最好用v-show,,v-if也可以
+        （会出现一种情况，如果在组讲根元素，用v-if,在mounted调用相应的函数，函数中牵扯到dom的话，
+        就会报出，应为v-if写在组件根源最外层，钩子函数韩式会执行的，蔼然没有Dom节点显示出来）
+        -->
+<!--    v-if="false"如果写在组件上或者组件里的根元素  两者的相同点 是组件标签都没有渲染-->
+    <none v-if="false"></none>
+
     <h1>我是父组件</h1>
     <chil-large @open="open"/>
     <chil-small />
@@ -16,6 +25,7 @@
   import ChilLarge from "./ChilLarge"
   import ChilSmall from "./ChilSmall"
   import AsyncData from "./AsyncData"
+  import none from '@/view/none.vue'
 
   export default {
     name: 'HelloWorld',
@@ -33,7 +43,8 @@
       ChilLarge,
       ChilSmall,
       AsyncData,
-      List
+      List,
+      none
     },
     methods:{
       open(msg){
---
+++ src/view/none.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="false">
+    123
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "none.vue",
+    created() {
+      console.log('none',123)
+    },
+    mounted() {
+      console.log('none',456)
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
